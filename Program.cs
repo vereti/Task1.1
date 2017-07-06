64c8657533ac107c54e0440ee51bbc0e080a9a9b
vereti <peace.atomka@yandex.ru>
2017-07-06 20:23:43 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a; // длина
+            double b;// ширина
+            double c;// длина диагонали 
+            double d;// высота 
+            double V;// объем
+            Console.Write("Введите длину стороны: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину стороны: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину диагонали грани: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            d = Math.Pow((Math.Pow(c, 2) - Math.Pow(a, 2)), 0.5);
+            V = a * b * d;
+
+            Console.WriteLine("Объем параллелепипеда равен: {0}", V);
+            Console.ReadKey();
+        }
+    }
+
+}
